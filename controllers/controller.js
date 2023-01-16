88a40611695cc13db584bbe4e07b1b7965d9026f
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-01-16 15:58:40 +0000

    update to controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,8 @@
 const { fetchTopics } = require("../models/model.js");
 
 const getTopics = (request, response, next) => {
-  console.log("hello world");
   fetchTopics()
     .then((topics) => {
-      console.log(topics);
       response.status(200).send(topics.rows);
     })
     .catch((error) => {
