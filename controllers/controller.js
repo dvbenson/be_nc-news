f9987eb7c3eb66dc654e21ffcd40c17da0795c5c
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-01-17 15:47:01 +0000

    final commit for pull request

--- controllers/controller.js
+++ controllers/controller.js
@@ -30,7 +30,6 @@ const getArticleById = (request, response, next) => {
   const { article_id } = request.params;
   fetchArticleById(article_id)
     .then((article) => {
-      console.log(article);
       response.status(200).send(article);
     })
     .catch((error) => {
