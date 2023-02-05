e2b7a816d673cd8e0fb21e420a7da6f89e19ef9d
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-02-05 21:04:05 +0000

    day of troubleshooting

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api", getAllEndPoints);
-app.get("/api/topics", getTopics);
-app.get("/api/articles", getArticles);
-app.get("/api/articles/:article_id", getArticleById);
-app.get("/api/users", getUsers);
-app.get("/api/articles/:article_id/comments", getArticleComments);
-app.patch("/api/articles/:article_id", patchArticleVotes);
-app.post("/api/articles/:article_id/comments", postComments);
-app.delete("/api/comments/:comment_id", searchComments);
+app.get("/api", getAllEndPoints); //good
+app.get("/api/topics", getTopics); //good
+app.get("/api/articles", getArticles); //good
+app.get("/api/articles/:article_id", getArticleById); //good
+app.get("/api/users", getUsers); //good
+app.get("/api/articles/:article_id/comments", getArticleComments); //good
+app.patch("/api/articles/:article_id", patchArticleVotes); // "msg": "The request body must be structured as follows: { inc_votes: number_of_votes }"
+app.post("/api/articles/:article_id/comments", postComments); // msg: invalid comment format
+app.delete("/api/comments/:comment_id", searchComments); //good
 
 app.use((error, request, response, next) => {
   if (error.status && error.msg) {
