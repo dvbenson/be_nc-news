511279b22a362fca8405f0a16ef5f523d353219b
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-02-14 08:13:00 +0000

    task16 routers

---
+++ routes/articles-router.js
@@ -0,0 +1,20 @@
+const {
+  getArticles,
+  getArticleComments,
+  getArticleById,
+  patchArticleVotes,
+  postComments,
+} = require("../controllers/controller");
+const articlesRouter = require("express").Router();
+
+articlesRouter.get("/api/articles", getArticles);
+
+articlesRouter.get("/api/articles/:article_id", getArticleById);
+
+articlesRouter.patch("/api/articles/:article_id", patchArticleVotes);
+
+articlesRouter.get("/api/articles/:article_id/comments", getArticleComments);
+
+articlesRouter.post("/api/articles/:article_id/comments", postComments);
+
+module.exports = articlesRouter;
